c386b513ae429290ff84cce287009412ffa638ab
CajucomW <cajucom.w@gmail.com>
2020-04-01 21:30:08 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Word Count
+
+A simple, python-powered, word-counting app that reads in from a text file. I'll be trying to recreate an activity I learned from <a target="_blank" href="http://www.kickstartcoding.com">KickStart Coding</a>. 
+
+<!--To play the game:-->
+
+<!--- clone the repo-->
+<!--- in terminal run-->
+<!--    <code>python3 hangman.py</code>-->
+
+<h3>TODO:</h3>
+<h4>04/01/20</h4>
+<ul>
+    <li>for now, get it started (LOL!)</li>
---
+++ wordcount.py
@@ -0,0 +1,19 @@
+############################################
+#                Word Count                #
+############################################
+## A challenge from KickStart Coding Bootcamp!!!
+
+# Write an application that reads in a text file
+# (see the book text file found in the 4th activity), and generates a word
+# count for each word it encounters.
+
+# Full Requirements:
+# - Sort the output either alphabetically, or by count.
+# - Only print the top X number, e.g. top 100.
+# - It should ignore case and punctuation.
+# - Use string formatting methods to make the output easy to read.
+# - The output, for example, might be something like:
+#       the 1801
+#       and 910
+#        to 794
+#       ...etc
